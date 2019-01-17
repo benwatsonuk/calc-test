4120902cafe9ce9b52b2c8843ea270ab2ef04251
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-17 14:30:04 +0000

    Added screen and basic total state

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React, {Component} from 'react';
+import Screen from './components/Screen';
 import './App.scss';
 
 class App extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            total: 0
+        };
+    }
+
     render() {
         return (
             <div className="App">
-
+                <Screen total={this.state.total}/>
             </div>
         );
     }
---
+++ src/components/Screen.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Screen.scss";
+
+class Screen extends React.Component {
+    render() {
+        return (
+            <div className="screen">
+                {this.props.total}
+            </div>
+        );
+    }
+}
+
+export default Screen;
+
