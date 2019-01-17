3c1c37af23a185272e970a75dbdba5a537dbb970
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-17 22:25:14 +0000

    Housekeeping

--- src/logic/operations.js
+++ src/logic/operations.js
@@ -1,21 +1,17 @@
 import Big from 'big.js'
 
 export default function operators(numberOne, numberTwo, operation) {
-    operation = operation || false
+    operation = operation || false;
     switch (operation) {
         case 'plus':
-            return new Big(numberOne).plus(numberTwo).toString()
-            break;
+            return new Big(numberOne).plus(numberTwo).toString();
         case 'minus':
-            return new Big(numberOne).minus(numberTwo).toString()
-            break;
+            return new Big(numberOne).minus(numberTwo).toString();
         case 'divide':
-            return new Big(numberOne).div(numberTwo).toString()
-            break;
+            return new Big(numberOne).div(numberTwo).toString();
         case 'multiply':
-            return new Big(numberOne).times(numberTwo).toString()
-            break;
+            return new Big(numberOne).times(numberTwo).toString();
         default :
-            throw 'No operator passed to function'
+            throw {error: 'No operator passed to function'}
     }
 }
