0a8e2d651708b8fea4f60d938796148638d8592c
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-17 16:39:26 +0000

    Basic buttons added

--- src/App.js
+++ src/App.js
@@ -10,10 +10,39 @@ class App extends Component {
         };
     }
 
+    renderButton = function (value, type) {
+        return <button
+            className={"calc__button calc__button--" + type}>
+            {value}
+        </button>
+    }
+
     render() {
         return (
             <div className="App">
                 <Screen total={this.state.total}/>
+                <div className="allButtons">
+                    <div className="numericButtons">
+                        {this.renderButton('decimal', 'numeric')}
+                        {this.renderButton(0, 'numeric')}
+                        {this.renderButton(1, 'numeric')}
+                        {this.renderButton(2, 'numeric')}
+                        {this.renderButton(3, 'numeric')}
+                        {this.renderButton(4, 'numeric')}
+                        {this.renderButton(5, 'numeric')}
+                        {this.renderButton(6, 'numeric')}
+                        {this.renderButton(7, 'numeric')}
+                        {this.renderButton(8, 'numeric')}
+                        {this.renderButton(9, 'numeric')}
+                    </div>
+                    <div className="operatorButtons">
+                        {this.renderButton('plus', 'operator')}
+                        {this.renderButton('minus', 'operator')}
+                        {this.renderButton('multiply', 'operator')}
+                        {this.renderButton('divide', 'operator')}
+                        {this.renderButton('equals', 'operator')}
+                    </div>
+                </div>
             </div>
         );
     }
