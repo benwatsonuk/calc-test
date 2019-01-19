65a596d6c59cd12c6edb8c5f826ced624e2a8846
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-19 07:58:43 +0000

    Removed the decimal button for now

--- src/__tests__/Buttons.test.js
+++ src/__tests__/Buttons.test.js
@@ -7,7 +7,7 @@ import Buttons from '../components/Buttons';
 describe('<Buttons />', () => {
     it('renders the 16 buttons', () => {
         const wrapper = shallow(<Buttons />);
-        expect(wrapper.find('.calc__button')).to.be.lengthOf(16);
+        expect(wrapper.find('.calc__button')).to.be.lengthOf(15);
     })
 
     // @todo tests for onClick events
