166408d5cbfcb177181dbf06728917168721b38b
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-21 21:45:49 +0000

    Fixed Screen test

--- src/__tests__/Screen.test.js
+++ src/__tests__/Screen.test.js
@@ -1,13 +1,17 @@
 import React from 'react';
 import {expect} from 'chai';
 import {shallow, mount, render} from 'enzyme';
-import sinon from 'sinon'
 import Screen from '../components/Screen';
 
 describe('<Screen />', () => {
     it('renders a screen with prop (total) value', () => {
         const wrapper = shallow(<Screen total="10"/>);
         expect(wrapper.find('.screen')).to.have.lengthOf(1);
-        expect(wrapper.props().children).to.equal('10');
+        expect(wrapper.props().children[1]['props'].children).to.equal('10');
+    })
+    it('renders a screen with EE branding', () => {
+        const wrapper = shallow(<Screen total="10"/>);
+        expect(wrapper.find('.screen')).to.have.lengthOf(1);
+        expect(wrapper.props().children[0]['props'].children.type).to.equal('img');
     })
 });
