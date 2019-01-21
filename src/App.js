86fa328297239da9ba4bdf9179dade40b554b56f
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-21 13:00:57 +0000

    App merged

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
 import React, {Component} from 'react';
 import Screen from './components/Screen';
+import Buttons from './components/Buttons';
+import operations from './logic/operations'
 import './App.scss';
 
 class App extends Component {
@@ -7,54 +9,65 @@ class App extends Component {
         super(props);
         this.state = {
             total: 0,
-            lastButtonType: null
+            lastButtonType: null,
+            operator: null,
+            numberToOperateWith: null
         };
+    }
+
+    clearAll = () => {
+        this.setState({
+            total: 0,
+            lastButtonType: null,
+            operator: null,
+            numberToOperateWith: null
+        });
     }
 
     handleClick = (value, type) => {
         if (type === 'numeric') {
-            this.setState({lastButtonType:type})
-        } else if (type === 'operator' && value === 'equals') {
-            this.setState({lastButtonType:null})
-        } else {
-            this.setState({lastButtonType:type})
+            if (this.state.operator === null) {
+                if (this.state.numberToOperateWith !== null) {
+                    this.setState({total: this.state.numberToOperateWith.toString() + value.toString()})
+                } else {
+                    this.setState({total: value.toString()})
+                }
+            }
+            this.setState({lastButtonType: type})
+            if (this.state.lastButtonType === 'numeric') {
+                if (this.state.numberToOperateWith !== 0) {
+                    this.setState({numberToOperateWith: this.state.numberToOperateWith.toString() + value.toString()})
+                } else {
+                    this.setState({numberToOperateWith: value})
+                }
+            } else {
+                this.setState({numberToOperateWith: value})
+            }
+        } else if (type === 'operator') {
+            if (value === 'equals') {
+                if (this.state.operator !== null) {
+                    this.setState({total: operations(this.state.total, this.state.numberToOperateWith, this.state.operator)})
+                }
+            } else if (value === 'cancel') {
+                this.clearAll()
+            } else {
+                this.setState({
+                    operator: value,
+                    lastButtonType: type
+                })
+                if (this.state.numberToOperateWith !== null && this.state.operator !== null) {
+                    this.setState({total: operations(this.state.total, this.state.numberToOperateWith, this.state.operator)})
+                    this.setState({numberToOperateWith: null})
+                }
+            }
         }
-    }
-
-    renderButton = function (value, type) {
-        return <button
-            className={"calc__button calc__button--" + type}
-            onClick={this.handleClick.bind(this, value, type)}>
-            {value}
-        </button>
     }
 
     render() {
         return (
             <div className="App">
                 <Screen total={this.state.total}/>
-                <div className="allButtons">
-                    <div className="numericButtons">
-                        {this.renderButton('decimal', 'numeric')}
-                        {this.renderButton(0, 'numeric')}
-                        {this.renderButton(1, 'numeric')}
-                        {this.renderButton(2, 'numeric')}
-                        {this.renderButton(3, 'numeric')}
-                        {this.renderButton(4, 'numeric')}
-                        {this.renderButton(5, 'numeric')}
-                        {this.renderButton(6, 'numeric')}
-                        {this.renderButton(7, 'numeric')}
-                        {this.renderButton(8, 'numeric')}
-                        {this.renderButton(9, 'numeric')}
-                    </div>
-                    <div className="operatorButtons">
-                        {this.renderButton('plus', 'operator')}
-                        {this.renderButton('minus', 'operator')}
-                        {this.renderButton('multiply', 'operator')}
-                        {this.renderButton('divide', 'operator')}
-                        {this.renderButton('equals', 'operator')}
-                    </div>
-                </div>
+                <Buttons total={this.state.total} onClick={(value, type) => this.handleClick(value, type)}/>
             </div>
         );
     }
