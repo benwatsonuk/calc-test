69d8c81f6be139a9da4470731308484e308fa75a
Ben Watson <ben.watson@digital.hmrc.gov.uk>
2019-01-21 13:40:19 +0000

    EE branding added

--- src/components/Screen.js
+++ src/components/Screen.js
@@ -1,11 +1,17 @@
 import React from "react";
+import logo from "./logo.svg";
 import "./Screen.scss";
 
 class Screen extends React.Component {
     render() {
         return (
             <div className="screen">
-                {this.props.total}
+                <div className="screen__branding">
+                    <img src={logo} />
+                </div>
+                <div className="screen__display">
+                    {this.props.total}
+                </div>
             </div>
         );
     }
